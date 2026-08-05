c32e79de3effdb8148acc0051a352bf327fe0f1d
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-08-05 12:28:27 +0500

    feat(add): Question: Which country has the highest number of registered users?

--- 07_aggregation_grouping.mongodb.js
+++ 07_aggregation_grouping.mongodb.js
@@ -63,3 +63,25 @@ db.users.aggregate([
         }
     }
 ])
+
+
+// Question : Which country has the highest number of registered users?
+
+db.users.aggregate([
+    {
+        $group: {
+          _id: "$company.location.country",
+          userCount: {
+            $sum: 1
+          }
+        }
+    },
+    {
+        $sort: {
+        userCount: -1
+        }
+    },
+    {
+        $limit: 1
+    }
+])
